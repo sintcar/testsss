05588147506e9b66c7e2941d7538361f020bb0d5
sintcar <sintcar666@gmail.com>
2025-12-13 10:44:50 +0700

    Fix seed user password

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (email, password_hash, role) VALUES
-('owner@example.com', '$2y$12$P8oKkV7Kos9yuFm86x3w2e0dYZoM3hV1xgN74iSKV.Rb.GHE0HmQm', 'owner');
+('owner@example.com', '$2y$12$vHdj5IwEaYI7BhivDU3DL.mKH01Iz2Bp16RqCKHaFvGnWQAOKy.Mi', 'owner');
 
 INSERT INTO quests (name, duration, price_9_12, price_13_17, price_18_21, tea_room_price, tea_room_duration, is_active)
 VALUES ('Тестовый квест', 60, 5000, 6000, 7000, 2000, 60, 1);
